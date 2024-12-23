f93e9fdfcbd537ba5df7f5de96d839707b08f412
IliDevia <neiluamaibi@mail.ru>
2024-12-23 13:23:06 +0300

    Added models for SQLA

---
+++ app/models/todo.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, Integer, String, Boolean, Text, DateTime
+from app.models.base import Base
+from datetime import datetime
+
+
+class Task(Base):
+    __tablename__ = "task"
+
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    title = Column(String(60), index=True, nullable=False)
+    description = Column(Text)
+    is_completed = Column(Boolean, default=False)
+    created_at = Column(DateTime, default=datetime.now)
+    deadline = Column(DateTime, nullable=True)
