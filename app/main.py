1e9d03fe71f7135c9b4b36e8a903a66cd328f306
IliDevia <neiluamaibi@mail.ru>
2024-12-23 11:32:48 +0300

    Start

---
+++ app/main.py
@@ -0,0 +1,13 @@
+import uvicorn
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get('/', summary='Главная ручка')
+def root():
+    return {'msg': 'start'}
+
+
+if __name__ == '__main__':
+    uvicorn.run('main:app', reload=True, port=8080)
