0300ff261d123d56d687037da34c380c18fa8e84
IliDevia <neiluamaibi@mail.ru>
2024-12-28 18:43:00 +0300

    finished schemas

--- app/schemas/todo.py
+++ app/schemas/todo.py
@@ -2,21 +2,21 @@ from pydantic import BaseModel
 from datetime import datetime
 from typing import Optional
 
+
 class TaskBase(BaseModel):
     title: str
     description: Optional[str] = None
     is_completed: bool = False
     deadline: Optional[datetime] = None
 
+
 class TaskCreate(TaskBase):
-    pass
+    ...
+
 
 class TaskUpdate(TaskBase):
-    pass
+    ...
 
-class Task(TaskBase):
+
+class TaskDelete(BaseModel):
     id: int
-    created_at: datetime
-
-    class Config:
-        orm_mode = True
