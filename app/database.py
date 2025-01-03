bef091232e5e033ee312e1cb0800f602154f89e3
IliDevia <neiluamaibi@mail.ru>
2025-01-03 13:38:02 +0300

    added core

--- app/database.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-
-
-class Database:
-    DATABASE_URL = "postgresql://user:password@localhost/dbname"
-    engine = create_engine(DATABASE_URL, pool_pre_ping=True)
-    SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
--- app/routes/v1/task.py
+++ app/routes/v1/task.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from app.repositories.psql_repository import PsqlRepository
 from app.models.todo import Task
-from app.dependencies import get_db
+from app.core.dependencies import get_db
 
 
 tasks_router = APIRouter()
