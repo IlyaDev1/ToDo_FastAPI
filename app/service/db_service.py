840d9da88f3512bc44eddd7a616552d4be160f7d
IliDevia <neiluamaibi@mail.ru>
2025-01-16 09:22:54 +0300

    added inject in service

--- app/service/db_service.py
+++ app/service/db_service.py
@@ -1,13 +1,14 @@
-from app.repositories.impl.task_psql_repository import TaskPSQLRepository
+from app.repositories.task_repository import TaskRepository
 from app.models.task_model import TaskModel
 from app.core.dependencies import get_db
 from app.entities.task_entity import TaskEntity
 from fastapi import Depends
+from inject import instance
 
 
 class TaskService:
     def __init__(self) -> None:
-        self.task_psql_repo: TaskPSQLRepository = TaskPSQLRepository(Depends(get_db))
+        self.task_psql_repo: TaskRepository = instance(TaskRepository)
 
     def get_all_tasks(self) -> list[TaskEntity]:
         return self.task_psql_repo.get_all_tasks()
