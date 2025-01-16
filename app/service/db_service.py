e719e24b726f0645dc4773c0c980e555ccad109a
IliDevia <neiluamaibi@mail.ru>
2025-01-16 09:24:57 +0300

    fixed repo's name

--- app/service/db_service.py
+++ app/service/db_service.py
@@ -8,7 +8,7 @@ from inject import instance
 
 class TaskService:
     def __init__(self) -> None:
-        self.task_psql_repo: TaskRepository = instance(TaskRepository)
+        self.task_repo: TaskRepository = instance(TaskRepository)
 
     def get_all_tasks(self) -> list[TaskEntity]:
-        return self.task_psql_repo.get_all_tasks()
+        return self.task_repo.get_all_tasks()
