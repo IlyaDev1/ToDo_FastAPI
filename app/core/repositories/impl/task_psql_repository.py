bc04ad7dcb9a0e030b8ea5e6c4c32e215421f717
IliDevia <neiluamaibi@mail.ru>
2025-02-03 15:41:42 +0300

    deleted mappers module

--- app/core/repositories/impl/task_psql_repository.py
+++ app/core/repositories/impl/task_psql_repository.py
@@ -3,7 +3,17 @@ from sqlalchemy.orm import Session
 from app.core.models.task_model import TaskModel
 from ..task_repository import TaskEntity
 from typing import Type
-from app.core.mappers import map_task_model_to_entity
+
+
+def map_task_model_to_entity(task_instance: Type[TaskModel]) -> TaskEntity:
+    return TaskEntity(
+        id=task_instance.id,
+        title=task_instance.title,
+        description=task_instance.description,
+        is_completed=task_instance.is_completed,
+        created_at=task_instance.created_at,
+        deadline=task_instance.deadline,
+    )
 
 
 class TaskPSQLRepository(TaskRepository):
