93bd65838ff1d96d9b5d6fc5878ae0ca871bdf15
IliDevia <neiluamaibi@mail.ru>
2025-02-03 11:27:36 +0300

    created mapper

--- app/core/repositories/impl/task_psql_repository.py
+++ app/core/repositories/impl/task_psql_repository.py
@@ -2,7 +2,8 @@ from ..task_repository import TaskRepository
 from sqlalchemy.orm import Session
 from app.core.models.task_model import TaskModel
 from ..task_repository import TaskEntity
-from typing import Type, Union
+from typing import Type
+from app.core.mappers import map_task_model_to_entity
 
 
 class TaskPSQLRepository(TaskRepository):
@@ -12,26 +13,10 @@ class TaskPSQLRepository(TaskRepository):
 
     def get_all_tasks(self) -> list[TaskEntity]:
         tasks: list[Type[TaskModel]] = self.session_instance.query(self.model_class).all()
-        return [
-            TaskEntity(
-                id=task.id,
-                title=task.title,
-                description=task.description,
-                is_completed=task.is_completed,
-                created_at=task.created_at,
-                deadline=task.deadline,
-            )
-            for task in tasks]
+        return [map_task_model_to_entity(task) for task in tasks]
 
     def get_task_by_id(self, id_value: int) -> TaskEntity | None:
-        task = self.session_instance.query(self.model_class).get(id_value)
+        task: Type[TaskModel] | None = self.session_instance.query(self.model_class).get(id_value)
         if task:
-            return TaskEntity(
-                id=task.id,
-                title=task.title,
-                description=task.description,
-                is_completed=task.is_completed,
-                created_at=task.created_at,
-                deadline=task.deadline,
-            )
+            return map_task_model_to_entity(task)
         return None
