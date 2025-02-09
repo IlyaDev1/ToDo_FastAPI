fdfd22677cf27b880a861e56da6000df6c5d12ba
IliDevia <neiluamaibi@mail.ru>
2025-02-09 13:04:06 +0300

    fixed many errors

--- app/core/config.py
+++ app/core/config.py
@@ -6,12 +6,12 @@ load_dotenv()
 
 
 class Settings:
-    APP_NAME: str = getenv('APP_NAME')
-    API_V1_STR: str = getenv('API_V1_STR')
-    DATABASE_URL: str = getenv('DATABASE_URL')
-    DESCRIPTION: str = getenv('DESCRIPTION')
-    DEBUG: bool = getenv('DEBUG').lower() == 'true'
-    APP_CONTAINER_PORT: int = int(getenv('APP_CONTAINER_PORT'))
+    APP_NAME: str | None = getenv('APP_NAME')
+    API_V1_STR: str | None = getenv('API_V1_STR')
+    DATABASE_URL: str | None = getenv('DATABASE_URL')
+    DESCRIPTION: str | None = getenv('DESCRIPTION')
+    DEBUG: bool | None = getenv('DEBUG').lower() == 'false'
+    APP_CONTAINER_PORT: int | None = int(getenv('APP_CONTAINER_PORT'))
 
 
 settings = Settings()
