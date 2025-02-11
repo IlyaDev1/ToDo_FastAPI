755d42ef3273e27bf472614bdeafbe819bd159b0
IliDevia <neiluamaibi@mail.ru>
2025-02-11 21:06:38 +0300

    added default values to config and env example values

--- app/core/config.py
+++ app/core/config.py
@@ -6,12 +6,12 @@ load_dotenv()
 
 
 class Settings:
-    APP_NAME: str = getenv('APP_NAME')
-    API_V1_STR: str = getenv('API_V1_STR')
-    DATABASE_URL: str = getenv('DATABASE_URL')
-    DESCRIPTION: str = getenv('DESCRIPTION')
-    DEBUG: bool = getenv('DEBUG').lower() == 'false'
-    APP_CONTAINER_PORT: int = int(getenv('APP_CONTAINER_PORT'))
+    APP_NAME: str = getenv('APP_NAME', 'Calendar')
+    API_V1_STR: str = getenv('API_V1_STR', '/api/v1')
+    DATABASE_URL: str = getenv('DATABASE_URL', 'postgresql://postgres:postgres@db:5432/postgres')
+    DESCRIPTION: str = getenv('DESCRIPTION', 'API for managing tasks and users')
+    DEBUG: bool = getenv('DEBUG', 'false')
+    APP_CONTAINER_PORT: int = int(getenv('APP_CONTAINER_PORT', '8085'))
 
 
 settings = Settings()
