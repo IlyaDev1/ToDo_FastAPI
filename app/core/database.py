e9497e5212ffea0f4a9f9d94fc1bad1a4932a646
IliDevia <neiluamaibi@mail.ru>
2025-02-20 11:08:14 +0300

    fst version

--- app/core/database.py
+++ app/core/database.py
@@ -1,12 +1,8 @@
-from sqlalchemy import create_engine
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
 
 from .config import settings
 
+engine = create_async_engine(settings.DATABASE_URL, echo=False)
 
-class Database:
-    engine = create_engine(settings.DATABASE_URL, pool_pre_ping=True)
-    SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-
-database = Database()
+SessionLocal = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
--- app/core/repositories/impl/task_psql_repository.py
+++ app/core/repositories/impl/task_psql_repository.py
@@ -1,7 +1,6 @@
 from typing import Type
 
-from sqlalchemy.orm import Session
-
+from app.core.dependencies import get_db
 from app.core.entities.task_entity import TaskEntity
 from app.core.models.task_model import TaskModel
 
@@ -21,49 +20,53 @@ def map_task_model_to_entity(task_instance: TaskModel) -> TaskEntity:
 
 
 class TaskPSQLRepository(TaskRepository):
-    def __init__(self, session_instance: Session) -> None:
-        self.session_instance = session_instance
+    def __init__(self) -> None:
         self.model_class: Type[TaskModel] = TaskModel
 
     async def get_all_tasks(self) -> list[TaskEntity]:
-        tasks: list[TaskModel] = self.session_instance.query(self.model_class).all()
-        return [map_task_model_to_entity(task) for task in tasks]
+        async with get_db() as session_instance:
+            tasks: list[TaskModel] = await session_instance.query(
+                self.model_class
+            ).all()
+            return [map_task_model_to_entity(task) for task in tasks]
 
     async def get_task_by_id(self, id_value: int) -> TaskEntity | None:
-        task: TaskModel | None = self.session_instance.query(self.model_class).get(
-            id_value
-        )
-        if task:
-            return map_task_model_to_entity(task)
-        return None
+        async with get_db() as session_instance:
+            task: TaskModel | None = await session_instance.query(self.model_class).get(
+                id_value
+            )
+            if task:
+                return map_task_model_to_entity(task)
+            return None
 
     async def create_task(self, task: TaskDTO) -> TaskEntity:
-        task = TaskModel(
-            title=task.title, description=task.description, deadline=task.deadline
-        )
-        self.session_instance.add(task)
-        self.session_instance.commit()
-        return map_task_model_to_entity(task)
+        async with get_db() as session_instance:
+            task = TaskModel(
+                title=task.title, description=task.description, deadline=task.deadline
+            )
+            await session_instance.add(task)
+            await session_instance.commit()
+            return map_task_model_to_entity(task)
 
     async def delete_task_by_id(self, id_value: int) -> TaskEntity | None:
-        task_instance = await self.session_instance.query(self.model_class).get(
-            id_value
-        )
-        if task_instance:
-            task_entity_instance = map_task_model_to_entity(task_instance)
-            self.session_instance.delete(task_instance)
-            self.session_instance.commit()
-            return task_entity_instance
-        return None
+        async with get_db() as session_instance:
+            task_instance = await session_instance.query(self.model_class).get(id_value)
+            if task_instance:
+                task_entity_instance = map_task_model_to_entity(task_instance)
+                await session_instance.delete(task_instance)
+                await session_instance.commit()
+                return task_entity_instance
+            return None
 
     async def change_instance(self, task: TaskEntity) -> TaskEntity:
-        task_model_instance: TaskModel = await self.session_instance.query(
-            self.model_class
-        ).get(task.id)
-        task_model_instance.title = task.title
-        task_model_instance.description = task.description
-        task_model_instance.is_completed = task.is_completed
-        task_model_instance.created_at = task.created_at
-        task_model_instance.deadline = task.deadline
-        self.session_instance.commit()
-        return map_task_model_to_entity(task_model_instance)
+        async with get_db() as session_instance:
+            task_model_instance: TaskModel = await session_instance.query(
+                self.model_class
+            ).get(task.id)
+            task_model_instance.title = task.title
+            task_model_instance.description = task.description
+            task_model_instance.is_completed = task.is_completed
+            task_model_instance.created_at = task.created_at
+            task_model_instance.deadline = task.deadline
+            await session_instance.commit()
+            return map_task_model_to_entity(task_model_instance)
