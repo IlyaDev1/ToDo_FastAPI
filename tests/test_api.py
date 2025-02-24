205352c78047740fb0a64dee8791221bfcd5beaf
Ilya Konovalov <devkvln@yandex.ru>
2025-02-24 10:49:27 +0300

    test passed

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,3 +1,5 @@
+import os
+
 import pytest
 from httpx import ASGITransport, AsyncClient
 
@@ -6,6 +8,7 @@ from app.main import app
 
 @pytest.mark.asyncio
 async def test_get_all_tasks():
+    print(f"ASYNC_DATABASE_URL: {os.getenv('ASYNC_DATABASE_URL')}")
     async with AsyncClient(
         transport=ASGITransport(app=app), base_url="http://test"
     ) as ac:
