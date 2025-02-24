9ade9f2bb1ac6e592b5f382179a7ac012732d36f
Ilya Konovalov <devkvln@yandex.ru>
2025-02-24 11:02:11 +0300

    deleted env print

--- tests/test_api.py
+++ tests/test_api.py
@@ -8,7 +8,6 @@ from app.main import app
 
 @pytest.mark.asyncio
 async def test_get_all_tasks():
-    print(f"ASYNC_DATABASE_URL: {os.getenv('ASYNC_DATABASE_URL')}")
     async with AsyncClient(
         transport=ASGITransport(app=app), base_url="http://test"
     ) as ac:
