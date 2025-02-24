0e5c22287e35a72f54073f39c32670aaa25aca89
Ilya Konovalov <devkvln@yandex.ru>
2025-02-24 09:35:20 +0300

    fst version of a httpx test

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,15 +1,13 @@
-from dotenv import load_dotenv
-from requests import get  # type: ignore
+import pytest
+from httpx import ASGITransport, AsyncClient
 
-from app.core.config import settings
-from logger import logger
-
-load_dotenv()
-
-main_url = f"http://localhost:{settings.APP_HOST_PORT}{settings.API_V1_STR}/task"
-logger.debug(main_url)
+from app.main import app
 
 
-def test_get_all_tasks():
-    response = get(main_url)
-    assert response.status_code == 200
+@pytest.mark.asyncio
+async def test_get_all_tasks():
+    async with AsyncClient(
+        transport=ASGITransport(app=app), base_url="http://test"
+    ) as ac:
+        response = await ac.get("api/v1/task/")
+        assert response.status_code == 200
