a22978b0d177651434b6dba3b5f0d11a6d0a60e9
Ilya Konovalov <devkvln@yandex.ru>
2025-02-28 15:23:27 +0300

    finished

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,18 @@
+import os
+
 import pytest
+from dotenv import load_dotenv
 
 from app.core.database import engine
 from app.core.models.base import Base
 from app.core.models.task_model import TaskModel
 
+load_dotenv()
+
 
 @pytest.fixture(scope="session", autouse=True)
 async def setup_db():
+    assert os.getenv("MODE") == "TEST", "Ты берешь не тестовую БД"
     for i in range(100):
         print("test")
     async with engine.begin() as conn:
