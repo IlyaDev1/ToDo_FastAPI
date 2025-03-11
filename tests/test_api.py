5ba238cab8d7d04a19987df66bea4828b7bc5a4d
Ilya Konovalov <devkvln@yandex.ru>
2025-03-11 10:57:41 +0300

    fixed method async_client

--- tests/test_api.py
+++ tests/test_api.py
@@ -9,12 +9,13 @@ from app.main import app
 @pytest.mark.asyncio
 @pytest.mark.usefixtures("setup_db")
 class APITests:
-    async def async_client(self):
+    @staticmethod
+    async def async_client():
         """Возвращаем асинхронный клиент для асинхронных тестов"""
         return AsyncClient(transport=ASGITransport(app=app), base_url="http://test")
 
     async def test_service_is_alive(self):
         """Проверяем, что сервер запущен, отдает хоть что-то"""
-        async with self.async_client() as ac:
+        async with await self.async_client() as ac:
             response = await ac.get("/api/v1/task/")
             assert response.status_code == 200
