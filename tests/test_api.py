8f3d5fc9ef1c9a2aa18ab2991accf7580236d013
Ilya Konovalov <devkvln@yandex.ru>
2025-03-24 18:40:07 +0300

    deleted fixture

--- tests/test_api.py
+++ tests/test_api.py
@@ -8,15 +8,10 @@ from app.main import app
 
 @pytest.mark.asyncio
 class TestAPI:
-    @pytest.fixture()
-    async def get_async_client(self):
-        """Возвращаем асинхронный клиент для асинхронных тестов"""
+    async def test_service_is_alive(self):
+        """Проверяем, что сервер запущен, отдает хоть что-то"""
         async with AsyncClient(
             transport=ASGITransport(app=app), base_url="http://test"
         ) as ac:
-            yield ac
-
-    async def test_service_is_alive(self, async_client):
-        """Проверяем, что сервер запущен, отдает хоть что-то"""
-        response = await async_client.get("/api/v1/task/")
-        assert response.status_code == 200
+            response = await ac.get("/api/v1/task/")
+            assert response.status_code == 200
