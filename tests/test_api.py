3c8d4a7e002b71e141cca6126cb0eabcee01964e
Ilya Konovalov <devkvln@yandex.ru>
2025-04-20 13:33:19 +0300

    added fixture for create async httpx client

--- tests/test_api.py
+++ tests/test_api.py
@@ -7,9 +7,7 @@ from app.main import app
 
 
 @pytest.mark.asyncio
-async def test_get_all_tasks():
-    async with AsyncClient(
-        transport=ASGITransport(app=app), base_url="http://test"
-    ) as ac:
-        response = await ac.get("/api/v1/task/")
-        assert response.status_code == 200
+async def test_get_all_tasks(async_client):
+    """Это тест, который показывает, что система минимально работает"""
+    response = await async_client.get("/api/v1/task/")
+    assert response.status_code == 200
