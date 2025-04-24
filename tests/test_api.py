bd85e2e92636ea3633660cac4aebc892f0d9031d
Ilya Konovalov <devkvln@yandex.ru>
2025-04-24 15:57:33 +0300

    added test for too long (61) title

--- tests/test_api.py
+++ tests/test_api.py
@@ -32,3 +32,21 @@ class TestCreateTask:
         )
         response = await async_client.post("/api/v1/task/", json=payload.to_json())
         assert response.status_code == 422
+
+    @pytest.mark.asyncio
+    async def test_create_task_with_too_long_title(self, async_client: AsyncClient):
+        """Мы пытаемся создать задачу с title больше 60, так нельзя по нашей бузинес-логике"""
+        # Я ожидаю, что дропнется http со статус кодом 422, чтобы обработка была на уровне pydantic
+        payload: TaskDTO = (
+            TaskDTO(  # Я хочу создать в новом пуллрике фикстуру, отдающую payload, пока этого не делаю, так как хочу, чтобы пуллрики были максимально маленькими и соответствовали принципу единственной ответственности как бы говоря
+                title="a" * 61,
+                description="description",
+                deadline=datetime(2025, 10, 10, 5, 1, 1),
+                is_completed=False,
+                created_at=None,
+            )
+        )
+        response = await async_client.post(
+            "/api/v1/task/", json=payload.to_json()
+        )  # И еще я на уровне класса теста вынесу url в глобальную область видимости в некст пуллрике
+        assert response.status_code == 422
