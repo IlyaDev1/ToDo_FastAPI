e50c55c82cb03ac1c6d8dd45d222b15babc5b229
Ilya Konovalov <devkvln@yandex.ru>
2025-04-24 15:31:51 +0300

    payload now dto instead of dict

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,9 +1,11 @@
 import os
+from dataclasses import asdict
 from datetime import datetime
 
 import pytest
 from httpx import ASGITransport, AsyncClient
 
+from app.core.dtos.task_dto import TaskDTO
 from app.main import app
 
 
@@ -18,13 +20,15 @@ class TestCreateTask:
     """Класс тестов для создания задачи"""
 
     @pytest.mark.asyncio
-    async def test_create_task_with_empty_title(self, async_client):
+    async def test_create_task_with_empty_title(self, async_client: AsyncClient):
         """Мы пытаемся создать задачу с пустым title полем, так делать нельзя по нашей бизнес-логике"""
         # Я ожидаю http ответ со статус кодом 422, потому что нужна в таком случае обработка на уровне парсинга данных pydantic(ом)
-        payload: dict = {
-            "title": "",
-            "description": "str",
-            "deadline": datetime(2025, 10, 10, 5, 1, 1).isoformat(),
-        }
-        response = await async_client.post("/api/v1/task/", json=payload)
+        payload: TaskDTO = TaskDTO(
+            title="",
+            description="description",
+            deadline=datetime(2025, 10, 10, 5, 1, 1),
+            is_completed=False,
+            created_at=None,
+        )
+        response = await async_client.post("/api/v1/task/", json=payload.to_json())
         assert response.status_code == 422
