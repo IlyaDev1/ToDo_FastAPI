ab652e0309167374167a2573216b238928637829
Ilya Konovalov <devkvln@yandex.ru>
2025-04-27 14:17:16 +0300

    added consts

--- tests/test_api.py
+++ tests/test_api.py
@@ -8,16 +8,20 @@ from httpx import ASGITransport, AsyncClient
 from app.core.dtos.task_dto import TaskDTO
 from app.main import app
 
+task_url = "/api/v1/task/"
+
 
 @pytest.mark.asyncio
 async def test_get_all_tasks(async_client: AsyncClient):
     """Это тест, который показывает, что система минимально работает"""
-    response = await async_client.get("/api/v1/task/")
+    response = await async_client.get(task_url)
     assert response.status_code == 200
 
 
 class TestCreateTask:
     """Класс тестов для создания задачи"""
+
+    max_title_length = 60
 
     @pytest.mark.asyncio
     async def test_create_task_with_empty_title(self, async_client: AsyncClient):
@@ -30,37 +34,37 @@ class TestCreateTask:
             is_completed=False,
             created_at=None,
         )
-        response = await async_client.post("/api/v1/task/", json=payload.to_json())
+        response = await async_client.post(task_url, json=payload.to_json())
         assert response.status_code == 422
 
     @pytest.mark.asyncio
     async def test_create_task_with_too_long_title(self, async_client: AsyncClient):
         """Мы пытаемся создать задачу с title больше 60, так нельзя по нашей бузинес-логике"""
         # Я ожидаю, что дропнется http со статус кодом 422, чтобы обработка была на уровне pydantic
-        payload = TaskDTO(  # Я хочу создать в новом пуллрике фикстуру, отдающую payload, пока этого не делаю, так как хочу, чтобы пуллрики были максимально маленькими и соответствовали принципу единственной ответственности как бы говоря
-            title="a" * 61,
+
+        too_long_title = "a" * (TestCreateTask.max_title_length + 1)
+        payload = TaskDTO(
+            title=too_long_title,
             description="description",
             deadline=datetime(2025, 10, 10, 5, 1, 1),
             is_completed=False,
             created_at=None,
         )
-        response = await async_client.post(
-            "/api/v1/task/", json=payload.to_json()
-        )  # И еще я на уровне класса теста вынесу url в глобальную область видимости в некст пуллрике
+        response = await async_client.post(task_url, json=payload.to_json())
         assert response.status_code == 422
 
     @pytest.mark.asyncio
     async def test_create_task_with_60_title(self, async_client: AsyncClient):
         """Мы пытаемся создать задачу с title 60, так как это можно по нашей бузинес-логике (крайний случай)"""
         # Я ожидаю, что дропнется http со статус кодом 201
-        payload = TaskDTO(  # Я хочу создать в новом пуллрике фикстуру, отдающую payload, пока этого не делаю, так как хочу, чтобы пуллрики были максимально маленькими и соответствовали принципу единственной ответственности как бы говоря
-            title="a" * 60,
+
+        extreme_title = "a" * TestCreateTask.max_title_length
+        payload = TaskDTO(
+            title=extreme_title,
             description="description",
             deadline=datetime(2025, 10, 10, 5, 1, 1),
             is_completed=False,
             created_at=None,
         )
-        response = await async_client.post(
-            "/api/v1/task/", json=payload.to_json()
-        )  # И еще я на уровне класса теста вынесу url в глобальную область видимости в некст пуллрике
+        response = await async_client.post(task_url, json=payload.to_json())
         assert response.status_code == 201
