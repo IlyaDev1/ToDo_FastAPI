5ea478bce8875419cfecab74854d2d2ae2aa8874
Ilya Konovalov <devkvln@yandex.ru>
2025-05-12 20:21:35 +0300

    added constants module for pydantic

--- app/api/schemas/task.py
+++ app/api/schemas/task.py
@@ -2,13 +2,15 @@ from datetime import datetime
 
 from pydantic import BaseModel, Field
 
+from app.api.schemas.constants import MAX_DESCRIPTION_LENGTH, MAX_TITLE_LENGTH
+
 
 class TaskCreate(BaseModel):
     title: str = Field(
-        ..., max_length=60, min_length=1, description="Оглавление задачи"
+        ..., max_length=MAX_TITLE_LENGTH, min_length=1, description="Оглавление задачи"
     )
     description: str | None = Field(
-        None, max_length=2000, description="Подробное описание задачи"
+        None, max_length=MAX_DESCRIPTION_LENGTH, description="Подробное описание задачи"
     )
     deadline: datetime | None = Field(
         None, description="Время, до которого нужно сделать задачу"
